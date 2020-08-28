57f1c6edc950437c05cf4d016d9444c4ff206220
Nikki <nikki.satopdx@gmail.com>
2020-08-28 11:37:42 -0700

    inital commit

---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+   <h1>Hi from the Third page</h1>
+    <p>Welcome to page 2</p>
+  </Layout>
+)
+
+export default ThirdPage
